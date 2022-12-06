fbfdd1cc62d86a5f50199f9cc697dff6a364a3a2
Labbeti <etienne.labbe31@gmail.com>
2022-12-06 11:19:56 +0100

    Mod: Update functional __init__ imports.

--- src/aac_metrics/functional/__init__.py
+++ src/aac_metrics/functional/__init__.py
@@ -3,10 +3,24 @@
 
 from .bleu import bleu
 from .cider_d import cider_d
+from .evaluate import aac_evaluate, evaluate
+from .fense import fense
 from .meteor import meteor
 from .rouge_l import rouge_l
 from .spice import spice
-from .evaluate import evaluate, aac_evaluate
-from .mult_cands import mult_cands_metric
-from .spider_max import spider_max
 from .spider import spider
+from .spider_max import spider_max
+
+
+__all__ = [
+    "bleu",
+    "cider_d",
+    "aac_evaluate",
+    "evaluate",
+    "fense",
+    "meteor",
+    "rouge_l",
+    "spice",
+    "spider",
+    "spider_max",
+]
