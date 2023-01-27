e4f155b0259a95886d540579f2d5bf862c53bfb5
Labbeti <etienne.labbe31@gmail.com>
2023-01-27 13:30:55 +0100

    Mod: Update aac-metrics version.

--- src/aac_metrics/__init__.py
+++ src/aac_metrics/__init__.py
@@ -10,7 +10,7 @@ __author_email__ = "labbeti.pub@gmail.com"
 __license__ = "MIT"
 __maintainer__ = "Etienne Labbé (Labbeti)"
 __status__ = "Development"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 from .classes.base import AACMetric
