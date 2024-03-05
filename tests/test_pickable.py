040e3d1c70d2c26d5a76b0d3ba1d3856320ae7a8
Labbeti <etienne.labbe31@gmail.com>
2024-03-05 13:57:06 +0100

    Mod: Test Evaluate class directly in pickable unittest.

--- tests/test_pickable.py
+++ tests/test_pickable.py
@@ -3,22 +3,19 @@
 
 import pickle
 import unittest
-
 from unittest import TestCase
 
-from aac_metrics.classes.evaluate import _instantiate_metrics_classes
+from aac_metrics.classes.evaluate import Evaluate
 
 
-class TestCompare(TestCase):
+class TestPickle(TestCase):
     # Tests methods
     def test_pickle_dump(self) -> None:
-        metrics = _instantiate_metrics_classes("all")
-
-        for metric in metrics:
-            try:
-                pickle.dumps(metric)
-            except pickle.PicklingError:
-                self.assertTrue(False, f"Cannot pickle {metric.__class__.__name__}.")
+        metrics = Evaluate(metrics="all")
+        try:
+            pickle.dumps(metrics)
+        except pickle.PicklingError:
+            self.assertTrue(False, f"Cannot pickle {metrics.__class__.__name__}.")
 
 
 if __name__ == "__main__":
