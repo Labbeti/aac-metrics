aa4ea39d4a78b45aaa0d0cffb8f50c3544b4390a
Labbeti <etienne.labbe31@gmail.com>
2025-01-16 17:19:12 +0100

    Fix: MACE compat test by setting random seed before forward.

--- tests/test_compatibility_mace.py
+++ tests/test_compatibility_mace.py
@@ -3,6 +3,7 @@
 
 import importlib
 import logging
+import random
 import sys
 import unittest
 from functools import partial
@@ -23,10 +24,11 @@ class TestMACECompatibility(TestCase):
     def setUpClass(cls) -> None:
         method = "combined"
         echecker = "echecker_clotho_audiocaps_base"
-        device = "cpu"  # note: current original mace cannot be used on cuda, it only uses cpu due to a bug
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        # device = "cpu"
 
-        mace_fn = cls._get_src_mace()
-        src_mace = partial(mace_fn, method=method, echecker=echecker)
+        src_mace_fn = cls._get_src_mace()
+        src_mace_fn = partial(src_mace_fn, method=method, echecker=echecker)
 
         cls.new_mace = MACE(
             mace_method=method,
@@ -34,8 +36,9 @@ class TestMACECompatibility(TestCase):
             device=device,
             verbose=2,
             echecker=echecker,
+            seed=None,  # seed will be set just before testing to ensure same seed for src and new MACE
         )
-        cls.src_mace = src_mace
+        cls.src_mace = src_mace_fn
 
     @classmethod
     def _get_src_mace(cls) -> Callable:
@@ -47,7 +50,7 @@ class TestMACECompatibility(TestCase):
 
     # Tests methods
     def test_examples_mace(self) -> None:
-        audio_dpath = Path(__file__).parent.joinpath("mace", "mace_metric", "assets")
+        audio_dpath = Path(__file__).parent.joinpath("mace", "assets")
         candidates = ["a man is speaking", "rain falls"]
         mult_references = [
             [
@@ -63,11 +66,16 @@ class TestMACECompatibility(TestCase):
         ]
         audio_paths = list(map(str, audio_paths))
 
+        seed = random.randint(0, 2**16)
+
+        random.seed(seed)
         new_outs = self.new_mace(
             candidates=candidates,
             mult_references=mult_references,
             audio_paths=audio_paths,
         )
+
+        random.seed(seed)
         src_outs = self.src_mace(
             candidates=candidates,
             mult_references=mult_references,
@@ -78,8 +86,9 @@ class TestMACECompatibility(TestCase):
             assert isinstance(new_dict, dict)
             assert isinstance(src_dict, dict)
             assert set(new_dict.keys()) == set(src_dict.keys())
+
             equals = {k: torch.equal(new_dict[k], src_dict[k]) for k in new_dict.keys()}
-            assert all(equals.values()), f"{equals=}"
+            assert all(equals.values()), f"{equals=}; {new_dict=}; {src_dict=}"
 
 
 if __name__ == "__main__":
