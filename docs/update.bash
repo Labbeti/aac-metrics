00f88509bf40a6b4308365f8f8202fa152a1ddd7
Labbeti <labbeti.pub@gmail.com>
2026-03-22 16:04:42 +0100

    mod: update autodocs script.

--- docs/update.bash
+++ docs/update.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-pkg_name="aac_metrics"
+pkg_pyname="aac_metrics"
 
 docs_dpath=`dirname $0`
-cd "$docs_dpath"
+cd "${docs_dpath}"
 
-rm ${pkg_name}.*rst
-uv run sphinx-apidoc -e -M -o . ../src/${pkg_name} && uv run make clean && uv run make html
+rm ${pkg_pyname}*.rst
+uv run sphinx-apidoc -e -M -o . "../src/${pkg_pyname}" && uv run make clean && uv run make html
 
 exit 0
